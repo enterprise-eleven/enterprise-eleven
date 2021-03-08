989acd782fcbeb4501f196363bf2ac03ea7e876f
Shaun McNamee <ShaunMcNamee+github@gmail.com>
2021-03-08 07:46:07 -0600

    refactor: nav bar
    - use Chakra image instead of next

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Box, Stack, Link as ChakraLink } from '@chakra-ui/react'
+import { Box, Stack, Link as ChakraLink, Image } from '@chakra-ui/react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 export const NavBar = () => {
